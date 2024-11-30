6e678966a3f6ef70b3e3aca6f2f7d3274d050c29
pedro-brandt <pedro.2020107111@unicap.br>
2024-11-30 11:17:58 -0300

    initial

---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//app.listen(3000);
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`)})
