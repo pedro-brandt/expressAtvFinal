e438bab475c8424fabbcbc2ebbff522b0bd1d467
pedro-brandt <pedro.2020107111@unicap.br>
2024-11-30 19:24:15 -0300

    commit inicial

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
@@ -6,5 +6,26 @@ const PORT = process.env.PORT || 3000;
 
 //app.listen(3000);
 
+app.get('/', (request, response) => {
+    response.status(201).send({msg: "Olá, beleza? "})
+    })
+
+app.get('/api/usuarios', (request, response) => {
+    response.send([
+        {id: 1, Nome: "pedro", apelido: "Pedro"},
+        {id: 2, Nome: "luiz", apelido: "Luiz"},
+        {id: 3, Nome: "maria", apelido: "Ma"}
+    ])
+})
+
+app.get('/api/produtos', (request, response) => {
+    response.send([{id: 11, mome: "pera", preco: 2.00}])
+})
+    
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`)})
+
+
+//localhost:3000
+//localhost:3000/usuarios
+//localhost:3000/produtos
